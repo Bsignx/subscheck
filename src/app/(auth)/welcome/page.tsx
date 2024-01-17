75e4e36b84f5fa7ac3ee4e7f34fa936e83fed509
unknown <bsignx@gmail.com>
2024-01-17 15:39:01 -0300

    feat: implement welcome page

---
+++ src/app/(auth)/welcome/page.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { Logo } from '@/components/ui/logo'
+import { Typography } from '@/components/ui/typography'
+
+export default function Welcome() {
+  return (
+    <main className="relative flex flex-col items-center justify-between min-h-screen pt-16 pb-6 px-6 bg-[url(/images/welcome-bg.png)] bg-contain bg-no-repeat bg-[center_top_7rem] overflow-hidden">
+      <div
+        // blur circle bg effect
+        aria-hidden
+        className="opacity-25 filter blur-[8rem] absolute top-52 left-64 bg-[hsl(var(--primary))] w-96 h-96"
+      ></div>
+
+      <Logo />
+
+      <div>
+        <Typography variant="bodyMedium" className="text-center mx-4">
+          Track your subscriptions and get control over your finances.
+        </Typography>
+        <Button className="w-full mt-10" asChild>
+          <Link href="/register">Get started</Link>
+        </Button>
+        <Button variant="chameleon" className="w-full mt-4" asChild>
+          <Link href="/login">I have an account</Link>
+        </Button>
+      </div>
+    </main>
+  )
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          'text-primary-foreground bg-primary hover:bg-primary/90 rounded-full shadow-[0_8px_25px_0_rgba(102,108,255,0.50)]  bg-[radial-gradient(100%_0%_at_45%_100%,rgba(255,255,255,0.00)_43.25%,rgba(102,108,255,0.50)_100%))]',
+        chameleon:
+          'text-primary-foreground bg-[rgba(255,255,255,0.10)] hover:bg-[rgba(255,255,255,0.10)]/20 rounded-full',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
@@ -21,7 +24,7 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline'
       },
       size: {
-        default: 'h-10 px-4 py-2',
+        default: 'h-12 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10'
