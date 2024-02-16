a0f30ab9a26adb2fb9f2ef32293c0f1422fdc217
unknown <bsignx@gmail.com>
2024-02-15 22:09:00 -0300

    feat: add progress component

---
+++ src/components/ui/progress.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+import * as ProgressPrimitive from '@radix-ui/react-progress'
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    indicatorClassName?: string
+  }
+>(({ className, value, indicatorClassName, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      'relative h-[5px] w-full overflow-hidden rounded-sm bg-progress',
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className={cn(
+        'h-full w-full flex-1 bg-primary transition-all',
+        indicatorClassName
+      )}
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+))
+Progress.displayName = ProgressPrimitive.Root.displayName
+
+export { Progress }
