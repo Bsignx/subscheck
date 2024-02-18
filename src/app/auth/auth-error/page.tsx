a3917a6c4cd534ea78f8bfb61bed49956ecd14f8
unknown <bsignx@gmail.com>
2024-02-17 22:29:43 -0300

    feat: style auth error page

--- src/app/auth/auth-error/page.tsx
+++ src/app/auth/auth-error/page.tsx
@@ -1,7 +1,27 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { Logo } from '@/components/ui/logo'
+import { Typography } from '@/components/ui/typography'
+
 export default function AuthError() {
   return (
-    <div>
-      <h1>AuthError</h1>
-    </div>
+    <main className="flex flex-col items-center justify-between min-h-screen pt-16 pb-16 px-6">
+      <Logo />
+      <div className="w-full">
+        <Typography variant="h2" as="h1" className="text-center">
+          Something went wrong
+        </Typography>
+        <Typography
+          variant="bodyMedium"
+          className="mt-4 text-center text-tertiary-foreground"
+        >
+          We couldn&apos;t log you in. Please try again.
+        </Typography>
+      </div>
+      <Button className="w-full mt-6" asChild>
+        <Link href="/auth/login">Go back to login</Link>
+      </Button>
+    </main>
   )
 }
