31e6004188e9eb7085f58f825bc996c09ae3ac8f
unknown <bsignx@gmail.com>
2024-02-21 22:36:59 -0300

    feat: create circular progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,47 @@
-import Image from 'next/image'
+'use client'
+
+import Link from 'next/link'
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles
+} from 'react-circular-progressbar'
+
+import { Settings } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
-import { currentUser, signOut } from '@/lib/auth'
+import { Logo } from '@/components/ui/icons/logo'
 
-export default async function Home() {
-  const user = await currentUser()
-  console.log(user)
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hi there
-      <Button>Click me</Button>
-      <form
-        action={async () => {
-          'use server'
+    <main>
+      <div className="h-[50vh] w-full rounded-b-2xl bg-white bg-gradient-to-b from-[#111113] to-[#282833]">
+        <Button variant="link" className="absolute top-4 right-2" asChild>
+          <Link href="/settings">
+            <Settings size="24" className=" text-[#A2A2B5]" />
+          </Link>
+        </Button>
 
-          await signOut()
-        }}
-      >
-        <button>Sign out</button>
-      </form>
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="w-[286px]">
+            <CircularProgressbarWithChildren
+              value={85}
+              maxValue={100}
+              strokeWidth={4}
+              circleRatio={0.75}
+              className="drop-shadow-[0_0_5px_rgba(102,108,255,0.5)]"
+              styles={buildStyles({
+                trailColor: 'rgba(78, 78, 97, 0.2)',
+                pathColor: '#666CFF',
+                rotation: 1 / 2 + 1 / 8,
+                strokeLinecap: 'round'
+              })}
+            >
+              <Logo className="w-28 h-fit mx-auto" />
+            </CircularProgressbarWithChildren>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
